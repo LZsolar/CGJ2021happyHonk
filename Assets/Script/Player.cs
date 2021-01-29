8f8e253907659efe8a5d0bc4a944632552748a75
LZsolar <minecraftradioevil@gmail.com>
2021-01-29 09:04:42 +0700

    AddPlayerMovement
    - add Player movement
    - start UI phoneMenu(Unfin)

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    Vector2 mm;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mm.x = Input.GetAxis("Horizontal");
+
+        mm.y = Input.GetAxis("Vertical");
+
+        rb.MovePosition(rb.position + mm * speed * Time.deltaTime);
+    }
+}
