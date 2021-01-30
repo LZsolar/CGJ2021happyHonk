e5d39570584ed78dc763e00a3cbee663d59aacb5
LZsolar <minecraftradioevil@gmail.com>
2021-01-30 23:37:19 +0700

    new UI Phone
    - add phone mockup

--- Assets/Script/MenuPhone.cs
+++ Assets/Script/MenuPhone.cs
@@ -1,19 +1,18 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhoneMenu : MonoBehaviour
+public class MenuPhone : MonoBehaviour
 {
-    public Player PlayerScp;
-    public GameObject phone,PageInfo,PageNote,PageSetting;
-    public Button Info,Note,Setting;
-    public GameObject Nitem1;
-    private bool ActivePhone,ActivePage;
+    public GameObject phone,PageMain, PageInfo, PageNote, PageSetting;
+    public Button Info, Note, Setting;
+    private bool ActivePhone, ActivePage;
     // Start is called before the first frame update
     void Start()
     {
         phone.SetActive(false);
+        PageMain.SetActive(false);
         ActivePhone = false;
         ActivePage = false;
         PageInfo.SetActive(false);
@@ -27,47 +26,45 @@ public class PhoneMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.X) && !ActivePhone)
         {
             phone.SetActive(true);
+            PageMain.SetActive(true);
             Info.Select();
             ActivePhone = true;
-            PlayerScp.speed = 0;
         }
 
         else if (Input.GetKeyDown(KeyCode.X) && ActivePhone && !ActivePage)
         {
             phone.SetActive(false);
+            PageMain.SetActive(false);
             ActivePhone = false;
-            PlayerScp.speed = 5;
         }
-        else if(Input.GetKeyDown(KeyCode.X)) {
-            phone.SetActive(true);
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            PageMain.SetActive(true);
+
             ActivePage = false;
             PageInfo.SetActive(false);
             PageNote.SetActive(false);
             PageSetting.SetActive(false);
         }
 
-
-        if (PlayerScp.Fitem1) {
-            Nitem1.SetActive(true);
-        }
     }
 
     public void InfoOpen()
     {
+        PageMain.SetActive(false);
         PageInfo.SetActive(true);
-        phone.SetActive(false);
         ActivePage = true;
     }
     public void NoteOpen()
     {
+        PageMain.SetActive(false);
         PageNote.SetActive(true);
-        phone.SetActive(false);
         ActivePage = true;
     }
     public void SettingOpen()
     {
+        PageMain.SetActive(false);
         PageSetting.SetActive(true);
-        phone.SetActive(false);
         ActivePage = true;
     }
-}*/
+}
