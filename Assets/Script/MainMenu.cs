40955826a225cf059b9f7bac24a80cd17b2ede12
LZsolar <minecraftradioevil@gmail.com>
2021-01-30 13:44:16 +0700

    AddMenu
    - Add MainMenu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button stt;
+    public GameObject Cradit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stt.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            Cradit.SetActive(false);
+        }
+    }
+
+    public void CraditOpen()
+    {
+        Cradit.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void qquit()
+    {
+        Application.Quit();
+    }
+}
